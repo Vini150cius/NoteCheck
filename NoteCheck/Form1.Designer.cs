2d3533fa4c39840c9866bc15fb41c45761c183ac
Vinicius Porto <viniciusporto010@gmail.com>
2024-10-12 22:54:57 -0300

    Mexendo na navegação e adicionando fontes

--- NoteCheck/Form1.Designer.cs
+++ NoteCheck/Form1.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnFechar = new System.Windows.Forms.Button();
+            this.lblBemVindo = new System.Windows.Forms.Label();
             this.btnMini = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(72)))), ((int)(((byte)(95)))));
+            this.panel1.Controls.Add(this.lblBemVindo);
             this.panel1.Controls.Add(this.btnMini);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -44,6 +46,36 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1904, 48);
             this.panel1.TabIndex = 0;
+            // 
+            // lblBemVindo
+            // 
+            this.lblBemVindo.AutoSize = true;
+            this.lblBemVindo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lblBemVindo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBemVindo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblBemVindo.Location = new System.Drawing.Point(0, 0);
+            this.lblBemVindo.Margin = new System.Windows.Forms.Padding(0);
+            this.lblBemVindo.Name = "lblBemVindo";
+            this.lblBemVindo.Padding = new System.Windows.Forms.Padding(10, 10, 0, 0);
+            this.lblBemVindo.Size = new System.Drawing.Size(232, 30);
+            this.lblBemVindo.TabIndex = 3;
+            this.lblBemVindo.Text = "Seja bem vindo ao NoteCheck";
+            // 
+            // btnMini
+            // 
+            this.btnMini.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(72)))), ((int)(((byte)(95)))));
+            this.btnMini.FlatAppearance.BorderSize = 0;
+            this.btnMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMini.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMini.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMini.Location = new System.Drawing.Point(1599, 3);
+            this.btnMini.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMini.Name = "btnMini";
+            this.btnMini.Size = new System.Drawing.Size(172, 42);
+            this.btnMini.TabIndex = 2;
+            this.btnMini.Text = "Minimizar ➖";
+            this.btnMini.UseVisualStyleBackColor = false;
+            this.btnMini.Click += new System.EventHandler(this.btnMini_Click);
             // 
             // btnFechar
             // 
@@ -58,21 +90,7 @@
             this.btnFechar.TabIndex = 1;
             this.btnFechar.Text = "Fechar ❌\r\n";
             this.btnFechar.UseVisualStyleBackColor = false;
-            // 
-            // btnMini
-            // 
-            this.btnMini.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(72)))), ((int)(((byte)(95)))));
-            this.btnMini.FlatAppearance.BorderSize = 0;
-            this.btnMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMini.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMini.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnMini.Location = new System.Drawing.Point(1619, 3);
-            this.btnMini.Margin = new System.Windows.Forms.Padding(0);
-            this.btnMini.Name = "btnMini";
-            this.btnMini.Size = new System.Drawing.Size(152, 42);
-            this.btnMini.TabIndex = 2;
-            this.btnMini.Text = "Minimizar ➖";
-            this.btnMini.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // StartScreen
             // 
@@ -86,6 +104,7 @@
             this.Text = "Tela inicial";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -95,6 +114,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnFechar;
         private System.Windows.Forms.Button btnMini;
+        private System.Windows.Forms.Label lblBemVindo;
     }
 }
 
