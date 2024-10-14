b228a977fed6c7143e60be4fab72bda72a36810f
Vinicius Porto <viniciusporto010@gmail.com>
2024-10-13 22:33:22 -0300

    Fazendo tela de inicio

---
+++ NoteCheck/StartScreen.Designer.cs
@@ -0,0 +1,246 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NoteCheck
+{
+    partial class StartScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBack = new System.Windows.Forms.Panel();
+            this.pnlProgram = new System.Windows.Forms.Panel();
+            this.pnlControles = new System.Windows.Forms.Panel();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lblBoasVindas = new System.Windows.Forms.Label();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRetirar = new System.Windows.Forms.Button();
+            this.btnEntregar = new System.Windows.Forms.Button();
+            this.btnHistorico = new System.Windows.Forms.Button();
+            this.btnStatus = new System.Windows.Forms.Button();
+            this.pnlBack.SuspendLayout();
+            this.pnlProgram.SuspendLayout();
+            this.pnlControles.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlBack
+            // 
+            this.pnlBack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlBack.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlBack.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlBack.Controls.Add(this.pnlProgram);
+            this.pnlBack.Location = new System.Drawing.Point(0, 0);
+            this.pnlBack.Name = "pnlBack";
+            this.pnlBack.Size = new System.Drawing.Size(1082, 535);
+            this.pnlBack.TabIndex = 0;
+            this.pnlBack.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBack_Paint);
+            // 
+            // pnlProgram
+            // 
+            this.pnlProgram.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlProgram.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlProgram.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(80)))), ((int)(((byte)(105)))));
+            this.pnlProgram.Controls.Add(this.btnStatus);
+            this.pnlProgram.Controls.Add(this.btnHistorico);
+            this.pnlProgram.Controls.Add(this.btnEntregar);
+            this.pnlProgram.Controls.Add(this.lblBoasVindas);
+            this.pnlProgram.Controls.Add(this.btnRetirar);
+            this.pnlProgram.Controls.Add(this.pnlControles);
+            this.pnlProgram.Location = new System.Drawing.Point(57, 12);
+            this.pnlProgram.Name = "pnlProgram";
+            this.pnlProgram.Size = new System.Drawing.Size(969, 510);
+            this.pnlProgram.TabIndex = 2;
+            this.pnlProgram.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlProgram_Paint);
+            // 
+            // pnlControles
+            // 
+            this.pnlControles.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(73)))), ((int)(((byte)(96)))));
+            this.pnlControles.Controls.Add(this.pictureBox1);
+            this.pnlControles.Controls.Add(this.lblLogo);
+            this.pnlControles.Controls.Add(this.btnMinimizar);
+            this.pnlControles.Controls.Add(this.btnFechar);
+            this.pnlControles.Location = new System.Drawing.Point(0, 0);
+            this.pnlControles.Name = "pnlControles";
+            this.pnlControles.Size = new System.Drawing.Size(969, 40);
+            this.pnlControles.TabIndex = 2;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.btnMinimizar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMinimizar.Location = new System.Drawing.Point(870, 7);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(45, 27);
+            this.btnMinimizar.TabIndex = 1;
+            this.btnMinimizar.Text = "➖";
+            this.btnMinimizar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.btnFechar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFechar.Location = new System.Drawing.Point(921, 7);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(45, 27);
+            this.btnFechar.TabIndex = 0;
+            this.btnFechar.Text = "❌";
+            this.btnFechar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // lblBoasVindas
+            // 
+            this.lblBoasVindas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBoasVindas.AutoSize = true;
+            this.lblBoasVindas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.lblBoasVindas.Location = new System.Drawing.Point(405, 102);
+            this.lblBoasVindas.Name = "lblBoasVindas";
+            this.lblBoasVindas.Size = new System.Drawing.Size(86, 13);
+            this.lblBoasVindas.TabIndex = 1;
+            this.lblBoasVindas.Text = "Seja bem vindo!!";
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.lblLogo.Location = new System.Drawing.Point(53, 4);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(61, 13);
+            this.lblLogo.TabIndex = 2;
+            this.lblLogo.Text = "NoteCheck";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::NoteCheck.Properties.Resources.logoBeta;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 33);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRetirar
+            // 
+            this.btnRetirar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.btnRetirar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRetirar.Location = new System.Drawing.Point(296, 164);
+            this.btnRetirar.Name = "btnRetirar";
+            this.btnRetirar.Size = new System.Drawing.Size(377, 44);
+            this.btnRetirar.TabIndex = 4;
+            this.btnRetirar.Text = "Retirar notebooks";
+            this.btnRetirar.UseVisualStyleBackColor = false;
+            this.btnRetirar.Click += new System.EventHandler(this.btnRetirar_Click);
+            // 
+            // btnEntregar
+            // 
+            this.btnEntregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.btnEntregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntregar.Location = new System.Drawing.Point(296, 243);
+            this.btnEntregar.Name = "btnEntregar";
+            this.btnEntregar.Size = new System.Drawing.Size(377, 44);
+            this.btnEntregar.TabIndex = 5;
+            this.btnEntregar.Text = "Entregar notebooks";
+            this.btnEntregar.UseVisualStyleBackColor = false;
+            // 
+            // btnHistorico
+            // 
+            this.btnHistorico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.btnHistorico.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHistorico.Location = new System.Drawing.Point(296, 401);
+            this.btnHistorico.Name = "btnHistorico";
+            this.btnHistorico.Size = new System.Drawing.Size(377, 44);
+            this.btnHistorico.TabIndex = 6;
+            this.btnHistorico.Text = "Histórico de retirada";
+            this.btnHistorico.UseVisualStyleBackColor = false;
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(231)))), ((int)(((byte)(226)))));
+            this.btnStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatus.Location = new System.Drawing.Point(296, 322);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(377, 44);
+            this.btnStatus.TabIndex = 7;
+            this.btnStatus.Text = "Status dos notebooks";
+            this.btnStatus.UseVisualStyleBackColor = false;
+            // 
+            // StartScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 535);
+            this.Controls.Add(this.pnlBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StartScreen";
+            this.Text = "StartScreen";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.StartScreen_Load);
+            this.pnlBack.ResumeLayout(false);
+            this.pnlProgram.ResumeLayout(false);
+            this.pnlProgram.PerformLayout();
+            this.pnlControles.ResumeLayout(false);
+            this.pnlControles.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBack;
+        private System.Windows.Forms.Label lblBoasVindas;
+        private System.Windows.Forms.Panel pnlProgram;
+        private Panel pnlControles;
+        private Button btnFechar;
+        private Button btnMinimizar;
+        private Label lblLogo;
+        private PictureBox pictureBox1;
+        private Button btnRetirar;
+        private Button btnStatus;
+        private Button btnHistorico;
+        private Button btnEntregar;
+    }
+}
---
+++ NoteCheck/StartScreen.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteCheck
+{
+    public partial class StartScreen : Form
+    {
+        private PrivateFontCollection privateFonts = new PrivateFontCollection();
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeft,
+            int nTop,
+            int nRight,
+            int nBottom,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        private void LoadFontBebas()
+        {
+            string fontPath = @"./Bebas-Regular.ttf";
+            privateFonts.AddFontFile(fontPath);
+
+            lblLogo.Font = new Font(privateFonts.Families[0], 20, FontStyle.Regular);
+            lblBoasVindas.Font = new Font(privateFonts.Families[0], 20, FontStyle.Regular);
+
+        }
+        private void LoadFontLouis()
+        {
+            string fontPath = @"./Louis-George-Cafe-Bold.ttf";
+            privateFonts.AddFontFile(fontPath);
+
+            btnRetirar.Font = new Font(privateFonts.Families[1], 12, FontStyle.Bold);
+            btnEntregar.Font = new Font(privateFonts.Families[1], 12, FontStyle.Bold);
+            btnStatus.Font = new Font(privateFonts.Families[1], 12, FontStyle.Bold);
+            btnHistorico.Font = new Font(privateFonts.Families[1], 12, FontStyle.Bold);
+        }
+
+        public StartScreen()
+        {
+            InitializeComponent();
+            LoadFontBebas();
+            LoadFontLouis();
+
+        }
+        private void StartScreen_Load(object sender, EventArgs e)
+        {
+            btnRetirar.Region = Region.FromHrgn(CreateRoundRectRgn
+                (0,0,btnRetirar.Width,btnRetirar.Height, 40, 40));
+            btnEntregar.Region = Region.FromHrgn(CreateRoundRectRgn
+                (0, 0, btnEntregar.Width, btnEntregar.Height, 40, 40));
+            btnStatus.Region = Region.FromHrgn(CreateRoundRectRgn
+                (0, 0, btnStatus.Width, btnStatus.Height, 40, 40));
+            btnHistorico.Region = Region.FromHrgn(CreateRoundRectRgn
+                (0, 0, btnHistorico.Width, btnHistorico.Height, 40, 40));
+            pnlProgram.Region = Region.FromHrgn(CreateRoundRectRgn
+                (0, 0, pnlProgram.Width, pnlProgram.Height, 40, 40));
+        }
+
+        private void pnlBack_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnlProgram_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnRetirar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
